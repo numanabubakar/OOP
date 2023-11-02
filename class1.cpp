33ccccbcf17efdf23339a7d5365bee87aef2ab3d
Numan Abubakar <bhalonoman@gmail.com>
2023-11-03 02:00:05 +0500

    marks class

--- class1.cpp
+++ class1.cpp
@@ -1,22 +1,43 @@
 #include<iostream>
 using namespace std;
 
-class Test{
-int a;
+// class Test{
+// int a;
+// public:
+// void in(){
+//     cin>>a;
+// }
+// void out(){
+//     cout<<a;
+// }
+// };
+
+class Marks{
+int a,b,c;
 public:
 void in(){
-    cin>>a;
+    cout<<"Enter three marks : ";
+    cin>>a>>b>>c;
 }
-void out(){
-    cout<<a;
+int sum(){
+return a+b+c;
 }
+float avg(){
+    float avg = (a+b+c)/3;
+    return avg;
+}
+
 };
 int main(){
 
- Test test1;
- test1.out();
- test1.in();
- test1.out();
+//  Test test1;
+//  test1.in();
+//  test1.out();
 
+Marks obj1;
+obj1.in();
+float avg = obj1.avg();
+int sum = obj1.sum();
+cout<<"Sum is : "<<sum<<endl<<"Avg is : "<<avg;
     return 0;
 }
